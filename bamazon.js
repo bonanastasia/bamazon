26c12c74dff64a483b4a101b3208b30df10f05cf
Anastasia Marie <ana.goodwin10@gmail.com>
2017-04-21 23:08:24 -0500

    homework update

--- bamazon.js
+++ bamazon.js
@@ -1,79 +1,61 @@
-
-
 var prompt = require("prompt");
 var mysql = require("mysql");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "bamazon_db"
+    // Your password
+    password: "",
+    database: "bamazon_db"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 connection.query("SELECT * FROM products", function(err, res) {
-  if (err) throw err;
-  console.log(res);
-  prompt.start();
-  var schema = {
-    properties: {
-      item: {
-        description:"What would you like to buy (insert id)?"
-      },
-      quantity: {
-        description:"How many?",
-        type: "number",
-        message: "Must be a number"
-      }
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
     }
-  };
 
-  prompt.get(schema, function (err, result) {
-    console.log("You said you want to buy: " +result.quantity+ " of item " + result.item);
-  });
+    prompt.start();
+    var schema = {
+        properties: {
+            item: {
+                description: "What would you like to buy (insert id)?"
+            },
+            quantity: {
+                description: "How many?",
+                type: "number",
+                message: "Must be a number"
+            }
+        }
+    };
+
+    prompt.get(schema, function(err, result) {
+        console.log("You said you want to buy: " + result.quantity + " of item " + result.item);
+
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+
+
+
+
+            connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?', [result.quantity, result.item], function(error) {
+                if (error) {
+                    throw err;
+                }
+                console.log("Purchase made successfully");
+            });
+
+        });
+    });
 });
 
 
-
-// prompt.start();
-//   prompt.get({
-//     properties: {
-//       item: {
-//         description:"What is your name?"
-//       }
-//     }
-//   }, function (err, result) {
-//     console.log("You said you want to buy: " + result.item);
-//   });
-
-
-
-// connection.query("INSERT INTO products SET ?", {
-//   flavor: "Rocky Road",
-//   price: 3.00,
-//   quantity: 50
-// }, function(err, res) {});
-
-// connection.query("UPDATE products SET ? WHERE ?", [{
-//   quantity: 100
-// }, {
-//   flavor: "Rocky Road"
-// }], function(err, res) {});
-
-// connection.query("DELETE FROM products WHERE ?", {
-//   flavor: "strawberry"
-// }, function(err, res) {});
-
-// connection.query("SELECT * FROM products", function(err, res) {
-//   if (err) throw err;
-//   console.log(res);
-// });
